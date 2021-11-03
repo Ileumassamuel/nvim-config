eeccea5cf797ed23306b25019afdba4ffce0e0a4
Ileumassamuel <53974949+Ileumassamuel@users.noreply.github.com>
2021-11-03 12:18:54 -0400

    fixed gitsigns keybindings

--- lua/plugins/_gitsigns.lua
+++ lua/plugins/_gitsigns.lua
@@ -1,12 +1,4 @@
 require('gitsigns').setup {
-    -- signs = {
-        -- add          = {hl = 'DiffAdd'   , text = '│', numhl='GitSignsAddNr'},
-        -- change       = {hl = 'DiffChange', text = '│', numhl='GitSignsChangeNr'},
-        -- delete       = {hl = 'DiffDelete', text = '_', numhl='GitSignsDeleteNr'},
-        -- topdelete    = {hl = 'DiffDelete', text = '‾', numhl='GitSignsDeleteNr'},
-        -- changedelete = {hl = 'DiffChange', text = '~', numhl='GitSignsChangeNr'},
-    -- },
-    -- numhl = false,
     keymaps = {
         -- Default keymap options
         noremap = true,
@@ -16,18 +8,18 @@ require('gitsigns').setup {
         ['n [h'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>'"},
 
         -- ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+        ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
         -- ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
         -- ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+        ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+        -- ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
         -- ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-        -- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<CR>',
+        -- ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+        -- ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
+        -- ['n <leader>hU'] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
 
         -- Text objects
-        ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>',
-        ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<CR>'
+        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
     },
-    -- watch_index = {
-    --     interval = 1000
-    -- },
-    -- sign_priority = 6,
-    -- status_formatter = nil, -- Use default
 }
