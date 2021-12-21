498be059c6ca49bd8a77f79ef3a4a83c3f083f35
Ileumassamuel <53974949+Ileumassamuel@users.noreply.github.com>
2021-12-21 18:14:26 -0500

    moved bootstraper to init

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+-- Bootstrap packer if it ain't installed
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
 
